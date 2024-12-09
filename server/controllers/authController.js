aac7430e8bad21fdcf2ccdb75687dc8cb9ad1201
pavan347 <pavankumargarapati04@gmail.com>
2024-12-09 19:18:53 +0530

    logout controller added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/users.model.js";
 import bcrypt from "bcryptjs"
 import { jwt } from "jsonwebtoken";
+import { toUSVString } from "util";
 
 export const register = async (req, res) => {
 
@@ -71,6 +72,25 @@ export const login = async (req, res)=>{
         })
 
         return res.json({sucess:true});
+        
+    } catch (error) {
+        return res.json({success: false, message: error.message})
+    }
+}
+
+
+export const logout = async (req, res) =>{
+    try {
+
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite : process.env.NODE_ENV === "production" ? 'strict' : 'none',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        res.json({success: true, message: "Logged out successfully"});
+
         
     } catch (error) {
         return res.json({success: false, message: error.message})
