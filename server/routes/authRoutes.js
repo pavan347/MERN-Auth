f664d3ccc33c512fa389e0997ac9dc41b8bbbb40
pavan347 <pavankumargarapati04@gmail.com>
2024-12-12 13:39:46 +0530

    is-auth route added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, logout, sendVerificationOtp, verifyEmail } from "../controllers/authController.js";
+import { register, login, logout, sendVerificationOtp, verifyEmail, isAuthenticated } from "../controllers/authController.js";
 import userAuth from "../middlewares/userAuth.js";
 
 const authRouter = express.Router();
@@ -9,5 +9,6 @@ authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verify-otp", userAuth, sendVerificationOtp);
 authRouter.post("/verify-email", userAuth, verifyEmail); 
+authRouter.post("/is-auth", userAuth, isAuthenticated); 
 
 export default authRouter;
