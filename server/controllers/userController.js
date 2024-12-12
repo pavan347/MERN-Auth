f4ca00953e393f601689411c05cca44697435cd4
pavan347 <pavankumargarapati04@gmail.com>
2024-12-12 19:34:06 +0530

    user controller added (getUserdata)

---
+++ server/controllers/userController.js
@@ -0,0 +1,26 @@
+import userModel from "../models/users.model";
+
+export const getUserData = async (req, res) => {
+    try {
+
+        const { userId } = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+
+        res.json({
+            success: true,
+            userData: {
+                name: user.username,
+                email: user.email,
+                isAccountVerified: user.isAccountVerified
+            }
+        })
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
