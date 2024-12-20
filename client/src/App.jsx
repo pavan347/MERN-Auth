bf4667f2fa9d2276bd70e642f92d18f0877a6531
pavan347 <pavankumargarapati04@gmail.com>
2024-12-20 10:10:40 +0530

    toastify added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
 import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div>
+      <ToastContainer/>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
