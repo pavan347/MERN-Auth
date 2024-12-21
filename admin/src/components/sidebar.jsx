6bee463c9304793cbc30ca6d15059d1e9f398d1e
pavan347 <pavankumargarapati04@gmail.com>
2024-12-21 18:34:34 +0530

    uncessesary files deleted

--- admin/src/components/sidebar.jsx
+++ admin/src/components/sidebar.jsx
@@ -11,15 +11,15 @@ const Sidebar = () => {
     <ul className='text-[#515151] mt-5'>
 
         <NavLink to={'/admin-dashboard'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-48 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-          {/* <img className='min-w-5' src={assets.home_icon} alt='' /> */}
+          
           <p className='hidden md:block'>Dashboard</p>
         </NavLink>
         <NavLink to={'/all-users'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-          {/* <img className='min-w-5' src={assets.appointment_icon} alt='' /> */}
+          
           <p className='hidden md:block'>Users</p>
         </NavLink>
         <NavLink to={'/log-history'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9  cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
-          {/* <img className='min-w-5' src={assets.add_icon} alt='' /> */}
+         
           <p className='hidden md:block'>Log History</p>
         </NavLink>
       </ul>
