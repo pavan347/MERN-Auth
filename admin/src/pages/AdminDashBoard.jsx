d4c5adfc0008a563c12a68e75c3695e769a66c3a
pavan347 <pavankumargarapati04@gmail.com>
2024-12-21 12:42:17 +0530

    admin dashboard created

--- admin/src/pages/AdminDashBoard.jsx
+++ admin/src/pages/AdminDashBoard.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AdminDashBoard = () => {
   return (
-    <div>AdminDashBoard</div>
+    <div className='w-full flex flex-col items-center pt-32'>
+      <h1 className='text-4xl font-bold '><span className='text-blue-700'>Welcome</span> to Admin Dashboard <span className='text-blue-700'>!</span></h1>
+      <p className='text-sm mt-2'>Use the sidebar to navigate to different options.</p>
+    </div>  
   )
 }
 
