218dd769c3b9d760014e6b5944c96b06327cd0e8
pavan347 <pavankumargarapati04@gmail.com>
2024-12-21 12:43:47 +0530

    try catch added

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,48 +4,50 @@ import { toast } from "react-toastify";
 
 export const AppContext = createContext();
 
-const AppContextProvider = ( props ) => {
+const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : null
+  );
+  const [users, setUsers] = useState([]);
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : null);
-    const [users, setUsers] = useState([]);
+  const getAllUsers = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/user/get-all-users`, {
+        headers: { token: token },
+      });
 
-    const getAllUsers = async () => { 
-        const {data} = await axios.get(`${backendUrl}/api/user/get-all-users`, {headers: { token: token}});
-
-        if(data.success){
-            toast.success(data.message);
-            console.log(data.users);
-            setUsers(data.users);
-        }else{
-            toast.error(data.message);
-        }
-
+      if (data.success) {
+        toast.success(data.message);
+        console.log(data.users);
+        setUsers(data.users);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    const value = {
-        isLoggedIn,
-        setIsLoggedIn,
-        token,
-        setToken,
-        backendUrl,
-        users,
+  const value = {
+    isLoggedIn,
+    setIsLoggedIn,
+    token,
+    setToken,
+    backendUrl,
+    users,
+  };
+
+  useEffect(() => {
+    if (token) {
+      setIsLoggedIn(true);
+      getAllUsers();
     }
+  }, [token]);
 
-    useEffect(() => {
-        if(token){
-            setIsLoggedIn(true);
-            getAllUsers();
-        }
-    }, [token])
-  
   return (
-    <AppContext.Provider
-      value={value}
-    >
-      {props.children}
-    </AppContext.Provider>
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
 };
 
