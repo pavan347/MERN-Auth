a17ef8c73ac04ee166d6cbe665c2e8d75f9d1289
pavan347 <pavankumargarapati04@gmail.com>
2024-12-21 18:51:26 +0530

    assets import deleted

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react";
-import { assets } from "../assets/assets";
 import { navigation } from "../assets/data";
 import { FaBars } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
